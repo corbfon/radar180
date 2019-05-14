29ce9f392dc4e29720bc2e63b17000eabf6e10fc
corbfon <corbin.fonville@gmail.com>
2019-05-14 09:52:01 -0500

    index all seasons with ingestion

--- src/functions/ingest.ts
+++ src/functions/ingest.ts
@@ -6,15 +6,19 @@ import { retrieveGames } from './../services/nfl'
 import { gameFromRawGame } from '../model/game'
 import { teamsFromRawGame } from '../model/team'
 
+const availableSeasons = [2013, 2014, 2015, 2016, 2017, 2018]
+
 export const retrieveAndStoreGames = async () => {
   try {
-    const games = await retrieveGames({ season: 2017 })
-    let promises: Promise<Document>[] = []
-    games.forEach(game => {
-      promises.push(gameFromRawGame(game))
-      promises = promises.concat(teamsFromRawGame(game))
-    })
-    await Promise.all(promises)
+    for (let season of availableSeasons) {
+      const games = await retrieveGames({ season })
+      let promises: Promise<Document>[] = []
+      games.forEach(game => {
+        promises.push(gameFromRawGame(game))
+        promises = promises.concat(teamsFromRawGame(game))
+      })
+      await Promise.all(promises)
+    }
   } catch (err) {
     console.error(err)
   }
