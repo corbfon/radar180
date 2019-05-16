2372a1436a420aa14f79f1c402450f8fee159077
corbfon <corbin.fonville@gmail.com>
2019-05-16 01:22:22 -0500

    remove specialized avg_scores handler

--- src/functions/teams.ts
+++ src/functions/teams.ts
@@ -3,7 +3,7 @@ import { APIGatewayProxyHandler } from 'aws-lambda'
 
 // project imports
 import { handleHttpRequest } from './../utils/handle-http'
-import { Team, addByeWeeks, addGamesAvg } from '../model/team'
+import { Team } from '../model/team'
 import { withData, prepDataResponse } from './../middleware/http'
 import { withQueryValidator } from './../utils/validator'
 import { teamsQuery } from './../schemas/team'
@@ -11,13 +11,5 @@ import { teamsQuery } from './../schemas/team'
 export const retrieve: APIGatewayProxyHandler = handleHttpRequest([
   withQueryValidator(teamsQuery),
   withData(Team.withGames()),
-  prepDataResponse(),
-])
-
-export const retrieveSeasonAvg: APIGatewayProxyHandler = handleHttpRequest([
-  withQueryValidator(teamsQuery),
-  withData(Team.withGames()),
-  // addByeWeeks(),
-  // addGamesAvg(),
   prepDataResponse(),
 ])
